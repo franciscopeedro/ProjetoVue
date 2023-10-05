ca49e2143981a928e3324769fa6e2828e25b2ca5
Pedryn <franciscopedroficial@gmail.com>
2023-10-05 16:16:52 -0300

    cronometro adicionado

---
+++ src/components/Formulario.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para a criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+      <div class="column">
+        <Temporizador />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "FormulárioTarefas",
+  components: {
+    Temporizador,
+  },
+});
+</script>
+
+<style></style>
