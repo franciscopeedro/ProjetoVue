e77fad95fd6188bc3527851e6646f22ac0e8e2ad
Pedryn <franciscopedroficial@gmail.com>
2023-11-24 12:59:56 -0300

    atualização

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -4,7 +4,7 @@ import Cronometro from './Cronometro.vue';
     <div class="box has-text-weight-bold">
         <div class="columns">
             <div class="column is-7">
-                {{tarefa.descricao|| 'Tarefa sem descrição'}}
+                {{tarefa.descricao|| ' Tarefa sem descrição'}}
             </div>
             <div class="column">
                 <CronometroTempo :tempoEmSegundos="tarefa.duracaoEmSegundos"/>                
