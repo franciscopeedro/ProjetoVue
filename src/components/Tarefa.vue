d2f8ac868b3f3437020a8d9802c6e37cfbea7dfd
Pedryn <franciscopedroficial@gmail.com>
2023-11-28 12:48:45 -0300

    ajustes

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -4,7 +4,7 @@ import Cronometro from './Cronometro.vue';
     <div class="box has-text-weight-bold">
         <div class="columns">
             <div class="column is-7">
-                {{tarefa.descricao|| ' Tarefa sem descrição'}}
+                {{tarefa.descricao|| 'Tarefa sem descrição'}}
             </div>
             <div class="column">
                 <CronometroTempo :tempoEmSegundos="tarefa.duracaoEmSegundos"/>                
@@ -14,6 +14,7 @@ import Cronometro from './Cronometro.vue';
 </template>
 
 <script lang="ts">
+
 import { defineComponent, PropType } from 'vue';
 import CronometroTempo from './Cronometro.vue';
 import ITarefa from '@/interfaces/ITareda';
